85d97a4c1723ff5c9aca28ecba1329cb8952f4a8
Andi Muh. Yusran H <andimuhammaddyusranharis@gmail.com>
2026-04-19 22:03:01 +0800

    Membuat logika program luas dan keliling lingkaran

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
+//program menghitung luas dan keliling lingkaran
+//rumus luas = phi * r * r
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    float r, luas, keliling;
+    const float phi = 3.14;
+
+    cout << "Masukkan jari-jari lingkaran: ";
+    cin >> r;
+
+    luas = phi * r * r;
+    keliling = 2 * phi * r;
+
+    cout << "Luas lingkaran: " << luas << endl;
+    cout << "Keliling lingkaran: " << keliling << endl;
+
     return 0;
 }
