1493d85513f89acdc3ad3e063d340e7bc8c48fd5
nabilll <nabilimron06@gmail.com>
2026-04-21 05:35:23 +0800

    test commit

--- source/test.cpp
+++ source/test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main(){
+    
+    for (int i = 0; i < 10; i++){
+    cout<<"1"<<endl;
+    }
+
     cout << "Coba-Coba ji" << endl;
     cout << "Kenapa bukan namaku muncul" << endl;
     return 0;
