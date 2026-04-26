322c61b0614388b636d4a683ad9686945e2520af
unknown <rifyahalazizah21@gmail.com>
2026-04-26 22:37:06 +0800

    penambahan logika tambahRute untuk menghubungkan kota pada graph

--- source/graph_route.cpp
+++ source/graph_route.cpp
@@ -16,18 +16,22 @@ Graph* createGraph(int V) {
 }
 
 void tambahRute(Graph* graph, string asal, string tujuan, int jarak) {
-    // KODE AKAN DIKERJAKAN OLEH RIFYAH
-    if (graph->indeksKota.find(asal) == graph->indeksKota.end()){
+    if (graph->indeksKota.find(asal) == graph->indeksKota.end()) {
         int index = graph->indeksKota.size();
-        graph->indeksKota[asal]=index;
+        graph->indeksKota[asal] = index;
     }
 
-    if(graph->indeksKota.find(tujuan)==graph->indeksKota.end()){
+    if (graph->indeksKota.find(tujuan) == graph->indeksKota.end()) {
         int index = graph->indeksKota.size();
         graph->indeksKota[tujuan] = index;
-
     }
-    
+    int src = graph->indeksKota[asal];
+    int dest = graph->indeksKota[tujuan];
+
+    AdjNode* newNode = new AdjNode{tujuan, jarak, graph->head[src]};
+    graph->head[src] = newNode;
+    AdjNode* newNode2 = new AdjNode{asal, jarak, graph->head[dest]};
+    graph->head[dest] = newNode2;
 }
 
 void cetakRute(Graph* graph) {
