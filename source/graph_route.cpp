e2305b0f27a7da50660539f46ee959d1c34b3661
unknown <rifyahalazizah21@gmail.com>
2026-04-26 23:28:18 +0800

    implementasi fungsi cetakRute

--- source/graph_route.cpp
+++ source/graph_route.cpp
@@ -4,10 +4,8 @@ Graph* createGraph(int V) {
     // KODE AKAN DIKERJAKAN OLEH RIFYAH
     //syap
     Graph* graph = new Graph;
-    graph->jumlahKota = V;
-
-    graph->head = new AdjNode*[V];
-
+    graph->jumlahKota = V;//
+    graph->head = new AdjNode*[V];//
     for (int i=0; i<V; i++) {
         graph->head[i]=nullptr;
     }
@@ -19,23 +17,32 @@ void tambahRute(Graph* graph, string asal, string tujuan, int jarak) {
     if (graph->indeksKota.find(asal) == graph->indeksKota.end()) {
         int index = graph->indeksKota.size();
         graph->indeksKota[asal] = index;
-    }
-
+    }//
     if (graph->indeksKota.find(tujuan) == graph->indeksKota.end()) {
         int index = graph->indeksKota.size();
         graph->indeksKota[tujuan] = index;
-    }
+    }//
     int src = graph->indeksKota[asal];
-    int dest = graph->indeksKota[tujuan];
-
+    int dest = graph->indeksKota[tujuan];//
     AdjNode* newNode = new AdjNode{tujuan, jarak, graph->head[src]};
-    graph->head[src] = newNode;
+    graph->head[src] = newNode;//
     AdjNode* newNode2 = new AdjNode{asal, jarak, graph->head[dest]};
     graph->head[dest] = newNode2;
 }
 
 void cetakRute(Graph* graph) {
-    // KODE AKAN DIKERJAKAN OLEH RIFYAH
+    //syap
+    for (auto const& kota : graph->indeksKota) {
+        string namaKota = kota.first;
+        int index = kota.second; //
+        cout << namaKota << " -> "; //
+        AdjNode* temp = graph->head[index];
+        while (temp != nullptr) {
+            cout << "(" << temp->kotaTujuan << ", " << temp->jarak << " km) ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 }
 
 //LIST KOTA DAN KABUPATEN SULAWESI SELATAN
