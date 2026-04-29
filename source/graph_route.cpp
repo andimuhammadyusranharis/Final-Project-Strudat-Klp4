b7c34cf5b15bac4efdbdcd4481ade9a9ee95b7d9
unknown <rifyahalazizah21@gmail.com>
2026-04-29 09:59:01 +0800

    perbaikan rute graph + penyesuaian jarak

--- source/graph_route.cpp
+++ source/graph_route.cpp
@@ -1,15 +1,12 @@
 #include "graph_route.h"
 
 Graph* createGraph(int V) {
-    // KODE AKAN DIKERJAKAN OLEH RIFYAH
-    //syap
     Graph* graph = new Graph;
     graph->jumlahKota = V;//
     graph->head = new AdjNode*[V];//
     for (int i=0; i<V; i++) {
         graph->head[i]=nullptr;
     }
-
     return graph;
 }
 
@@ -55,60 +52,40 @@ void cetakRute(Graph* graph) {
     }
 }
 
-//LIST KOTA DAN KABUPATEN SULAWESI SELATAN
-//buat nanti ditaro di main.cpp bos
+//LIST KOTA DAN KABUPATEN SULAWESI SELATAN (buat nanti ditaro di main.cpp bos)
+//ctrl+/ 
 
-    // // HUB UTAMA: Kota Makassar
-    // tambahRute(graph, "Kota Makassar", "Kabupaten Gowa", 15);
-    // tambahRute(graph, "Kota Makassar", "Kabupaten Takalar", 36);
-    // tambahRute(graph, "Kota Makassar", "Kabupaten Maros", 33);
-    // tambahRute(graph, "Kota Makassar", "Kabupaten Pangkep", 49);
+//cout << "\nMenampilkan peta distribusi paket...\n";
+// Graph* graph = createGraph(100);
 
-    // // KABUPATEN GOWA
-    // tambahRute(graph, "Kabupaten Gowa", "Kabupaten Takalar", 30);
-    // tambahRute(graph, "Kabupaten Gowa", "Kabupaten Jeneponto", 86);
-    // tambahRute(graph, "Kabupaten Gowa", "Kabupaten Bantaeng", 136);
+ //// === JALUR BARAT (pesisir utama) ===
+// tambahRute(graph, "Kota Makassar", "Kabupaten Maros", 33);
+// tambahRute(graph, "Kabupaten Maros", "Kabupaten Pangkep", 20);
+// tambahRute(graph, "Kabupaten Pangkep", "Kabupaten Barru", 49);
+// tambahRute(graph, "Kabupaten Barru", "Kota Pare Pare", 64);
+// tambahRute(graph, "Kota Pare Pare", "Kabupaten Pinrang", 28);
 
-    // // KABUPATEN TAKALAR
-    // tambahRute(graph, "Kabupaten Takalar", "Kabupaten Jeneponto", 83);
+// // === JALUR TENGAH (naik ke utara) ===
+// tambahRute(graph, "Kota Pare Pare", "Kabupaten Sidrap", 65);
+// tambahRute(graph, "Kabupaten Sidrap", "Kabupaten Enrekang", 70); 
+// tambahRute(graph, "Kabupaten Enrekang", "Kabupaten Tana Toraja", 43);
+// tambahRute(graph, "Kabupaten Tana Toraja", "Kota Palopo", 95); 
 
-    // // KABUPATEN MAROS
-    // tambahRute(graph, "Kabupaten Maros", "Kabupaten Pangkep", 20);
-    // tambahRute(graph, "Kabupaten Maros", "Kabupaten Barru", 65);
+// // === JALUR TIMUR ===
+// tambahRute(graph, "Kabupaten Barru", "Kabupaten Soppeng", 65);
+// tambahRute(graph, "Kabupaten Soppeng", "Kabupaten Bone", 68);
+// tambahRute(graph, "Kabupaten Bone", "Kabupaten Wajo", 89);
+// tambahRute(graph, "Kabupaten Bone", "Kota Palopo", 65);
 
-    // // KABUPATEN PANGKEP
-    // tambahRute(graph, "Kabupaten Pangkep", "Kabupaten Barru", 49);
+// // === JALUR SELATAN ===
+// tambahRute(graph, "Kota Makassar", "Kabupaten Gowa", 15);
+// tambahRute(graph, "Kabupaten Gowa", "Kabupaten Takalar", 30);
+// tambahRute(graph, "Kabupaten Takalar", "Kabupaten Jeneponto", 83);
+// tambahRute(graph, "Kabupaten Jeneponto", "Kabupaten Bantaeng", 66);
+// tambahRute(graph, "Kabupaten Bantaeng", "Kabupaten Bulukumba", 40);
 
-    // // KABUPATEN BARRU
-    // tambahRute(graph, "Kabupaten Barru", "Kota Pare Pare", 64);
-    // tambahRute(graph, "Kabupaten Barru", "Kabupaten Soppeng", 65);
+// // === CABANG TAMBAHAN ===
+// tambahRute(graph, "Kabupaten Bulukumba", "Kabupaten Sinjai", 55);
+// tambahRute(graph, "Kabupaten Sinjai", "Kabupaten Bone", 60); 
 
-    // // KOTA PARE PARE (hub utara)
-    // tambahRute(graph, "Kota Pare Pare", "Kabupaten Pinrang", 28);
-    // tambahRute(graph, "Kota Pare Pare", "Kabupaten Sidrap", 65);
-
-    // // KABUPATEN PINRANG
-    // tambahRute(graph, "Kabupaten Pinrang", "Kabupaten Enrekang", 43);
-
-    // // KABUPATEN ENREKANG
-    // tambahRute(graph, "Kabupaten Enrekang", "Kabupaten Tana Toraja", 43);
-
-    // // KABUPATEN SIDRAP
-    // tambahRute(graph, "Kabupaten Sidrap", "Kabupaten Soppeng", 87);
-
-    // // KABUPATEN SOPPENG
-    // tambahRute(graph, "Kabupaten Soppeng", "Kabupaten Bone", 68);
-
-    // // KABUPATEN BONE
-    // tambahRute(graph, "Kabupaten Bone", "Kabupaten Wajo", 89);
-    // tambahRute(graph, "Kabupaten Bone", "Kota Palopo", 65);
-
-    // // KOTA PALOPO (timur Sulsel)
-    // tambahRute(graph, "Kota Palopo", "Kabupaten Luwu", 47);
-
-    // // KABUPATEN BULUKUMBA (selatan)
-    // tambahRute(graph, "Kabupaten Bulukumba", "Kabupaten Bantaeng", 40);
-    // tambahRute(graph, "Kabupaten Bulukumba", "Kabupaten Sinjai", 55);
-
-    // // KABUPATEN BANTAENG
-    // tambahRute(graph, "Kabupaten Bantaeng", "Kabupaten Jeneponto", 66);
+//cetakRute(graph);
