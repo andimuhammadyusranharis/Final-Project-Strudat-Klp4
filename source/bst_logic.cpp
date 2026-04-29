79047661cfdff707af224d8c78f13457a30bf7ba
nabilimron <nabilimron06@gmail.com>
2026-04-29 08:23:08 +0800

    Manambahkan Fungsi insertBST

--- source/bst_logic.cpp
+++ source/bst_logic.cpp
@@ -1,11 +1,27 @@
 #include "bst_logic.h"
 
 NodeBST* insertBST(NodeBST* root, Paket data) {
-    // KODE AKAN DIKERJAKAN OLEH NABIL
+    // Posisi kosong → buat node baru 
+    if (root == nullptr){
+        NodeBST* baru = new NodeBST();
+        baru->data = data;
+        baru->left = nullptr;
+        baru->right = nullptr;
+        return baru;
+    } 
+    
+    // Rekursi kiri atau kanan berdasarkan string resi 
+    if (data.resi < root->data.resi){
+        root->left = insertBST(root->left, data);
+    } else if (data.resi > root->data.resi){
+        root->right = insertBST(root->right, data);
+    } else { 
+        cout << "[!] Resi \"" << data.resi << "\" sudah ada!\n"; 
+    }
     return root;
 }
 
 NodeBST* searchBST(NodeBST* root, string resi) {
-    // KODE AKAN DIKERJAKAN OLEH NABIL
+    
     return nullptr;
 }
