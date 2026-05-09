671d7cec82c32224ab6de999faf4a34711b75132
nabilimron <nabilimron06@gmail.com>
2026-05-09 18:56:09 +0800

    Menambahkan Fungsi insertionsort

--- source/sort_search.cpp
+++ source/sort_search.cpp
@@ -1,7 +1,16 @@
 #include "sort_search.h"
 
 void insertionSortPaket(Paket arr[], int n) {
-    // KODE AKAN DIKERJAKAN OLEH ILHAM
+    for (int i = 1; i < n; i++) {
+        Paket key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j].prioritas > key.prioritas) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 void mergeSortPaket(Paket arr[], int l, int r) {
