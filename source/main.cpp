6d27c4a764f1785b1d25433f08a09cd315f442c7
veradelimah <veradelimah@gmail.com>
2026-05-09 15:41:36 +0800

    Menambahkan menu tambah paket menggunakan linked list dan BST

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,11 @@
 int main() {
     NodeBST* databaseResi = nullptr;
     NodeLL* antreanPaket = nullptr;
+
+     Paket dataPaket[100];
+    int jumlahPaket = 0;
+
+    Graph* graph = createGraph(5);
     
     int pilihan;
     do {
@@ -19,9 +24,26 @@ int main() {
         cout << "Pilih: "; cin >> pilihan;
 
         switch(pilihan) {
-            case 1: 
-                // Fungsi Vera
+            case 1: {
+                Paket p;
+                cout << "Masukkan Resi: "; cin >> p.resi;
+                cout << "Nama Pengirim: "; cin >> p.pengirim;
+                cout << "Tujuan: "; cin >> p.tujuan;
+                cout << "Berat (kg): "; cin >> p.berat;
+                cout << "Prioritas (1-5): "; cin >> p.prioritas;
+
+                // Masuk ke Linked List
+                tambahPaket(antreanPaket, p);
+
+                // Masuk ke BST
+                databaseResi = insertBST(databaseResi, p);
+
+                // Simpan ke array
+                dataPaket[jumlahPaket++] = p;
+
                 break;
+            }
+
             case 2:
                 // Panggil fungsi Nabil & Ilham
                 break;
