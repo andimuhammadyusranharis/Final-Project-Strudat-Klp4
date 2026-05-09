4ac7fb6671f608e39044a80b3e8d188eee6bf59f
nabilimron <nabilimron06@gmail.com>
2026-05-09 19:17:13 +0800

    Menambahkan Fungsi merge

--- source/sort_search.cpp
+++ source/sort_search.cpp
@@ -13,6 +13,29 @@ void insertionSortPaket(Paket arr[], int n) {
     }
 }
 
+void merge(Paket arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    Paket L[100], R[100];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i].berat <= R[j].berat) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
 void mergeSortPaket(Paket arr[], int l, int r) {
     // KODE AKAN DIKERJAKAN OLEH ILHAM
 }
