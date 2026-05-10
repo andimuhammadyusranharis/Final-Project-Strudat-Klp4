cdc2cac37ee8d30fb74e058b6cd1e2a8e3a7aeec
ilhameee <syahririlham690@gmail.com>
2026-05-10 10:41:31 +0800

    memperbaiki kode pada sort search

--- source/sort_search.cpp
+++ source/sort_search.cpp
@@ -1,11 +1,60 @@
 #include "sort_search.h"
-// 1. Binary Search (untuk mencari berdasarkan Resi)
+
+// Insertion Sort (berdasarkan prioritas)
+void insertionSortPaket(Paket arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        Paket key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j].prioritas > key.prioritas) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Merge function
+void merge(Paket arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    Paket L[100], R[100];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i].berat <= R[j].berat) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+// Merge Sort (berdasarkan berat)
+void mergeSortPaket(Paket arr[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+        mergeSortPaket(arr, l, m);
+        mergeSortPaket(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+// Binary Search (resi)
 int binarySearchPaket(Paket arr[], int n, string targetResi) {
     int left = 0, right = n - 1;
-    
+
     while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
+        int mid = (left + right) / 2;
+
         if (arr[mid].resi == targetResi) {
             return mid;
         } else if (arr[mid].resi < targetResi) {
@@ -17,10 +66,9 @@ int binarySearchPaket(Paket arr[], int n, string targetResi) {
     return -1;
 }
 
-// 2. Linear Search (untuk mencari berdasarkan nama pengirim)
+// Linear Search (nama pengirim)
 int linearSearchPaket(Paket arr[], int n, string targetNama) {
     for (int i = 0; i < n; i++) {
-        // Pastikan menggunakan .pengirim, sesuai dengan global.h
         if (arr[i].pengirim == targetNama) {
             return i;
         }
--- source/sort_search.h
+++ source/sort_search.h
@@ -1,20 +1,13 @@
 #ifndef SORT_SEARCH_H
 #define SORT_SEARCH_H
+#include "global.h"
 
-#include "global.h" // Mengambil definisi struct Paket dari global.h
-
-// --- PROTOTIPE FUNGSI SORTING ---
-// Mengurutkan array paket menggunakan Insertion Sort
+// Sorting: Mengurutkan array paket berdasarkan berat atau prioritas
 void insertionSortPaket(Paket arr[], int n);
-
-// Mengurutkan array paket menggunakan Merge Sort
 void mergeSortPaket(Paket arr[], int l, int r);
 
-// --- PROTOTIPE FUNGSI SEARCHING ---
-// Mencari paket berdasarkan Resi (menggunakan Binary Search - Array harus terurut)
+// Searching: Mencari data di dalam array yang sudah tersortir
 int binarySearchPaket(Paket arr[], int n, string targetResi);
-
-// Mencari paket berdasarkan Nama Pengirim (menggunakan Linear Search)
 int linearSearchPaket(Paket arr[], int n, string targetNama);
 
 #endif
